6b60d598077a5b93dcedb5386dac4c93a9166def
SenuraJayadeva <senurajayadeva@gmail.com>
2021-01-26 23:58:34 +0530

    completed crud backend

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -38,7 +38,75 @@ const getAllStudents = async (req, res, next) => {
   }
 };
 
+const getStudent = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const student = await firestore.collection("students").doc(id);
+    const data = await student.get();
+    if (data.empty) {
+      res.status(404).send("No Student Record Found");
+    } else {
+      res.send(data.data());
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const getStudentByITnumber = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const student = await firestore
+      .collection("students")
+      .where("itnum", "==", id);
+    const data = await student.get();
+    const studentsArray = [];
+    if (data.empty) {
+      res.status(404).send("No Student Record Found");
+    } else {
+      data.forEach((doc) => {
+        const student = new Student(
+          doc.id,
+          doc.data().itnum,
+          doc.data().name,
+          doc.data().age
+        );
+        studentsArray.push(student);
+      });
+      res.send(studentsArray);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const updateStudent = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const student = await firestore.collection("students").doc(id);
+    await student.update(data);
+    res.send("Student record updated successfully");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const deleteStudent = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await firestore.collection("students").doc(id).delete();
+    res.send("Student record deleted successfully");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
 module.exports = {
   addStudent,
   getAllStudents,
+  getStudent,
+  getStudentByITnumber,
+  updateStudent,
+  deleteStudent,
 };
--- routes/student.route.js
+++ routes/student.route.js
@@ -2,11 +2,19 @@ const express = require("express");
 const {
   addStudent,
   getAllStudents,
+  getStudent,
+  getStudentByITnumber,
+  updateStudent,
+  deleteStudent,
 } = require("../controllers/student.controller");
 
 const router = express.Router();
 
 router.post("/add", addStudent);
 router.get("/", getAllStudents);
+router.get("/:id", getStudent);
+router.get("/findbyitnum/:id", getStudentByITnumber);
+router.put("/update/:id", updateStudent);
+router.delete("/:id", deleteStudent);
 
 module.exports = router;
